86479e9b3c7e70babfc3d659571dfa7f571d3978
SebastianProg <66634871+SebastianProg@users.noreply.github.com>
2021-01-25 22:11:06 +0100

    Delete src/Plotting directory

--- src/Plotting/__init__.py
+++
@@ -1,3 +0,0 @@
-from .general import closeAll, figure, plotData, plotErrData, plotDictData, surfPlot, parulaColMap
-from .specific import stereoProjPlot, plotPeakPositions1, plotSin2Psi, plotMultiWavelength, plotUniversalPlot, \
-	plotStresses, plotStrainFreeLatticeSpacing, plotInverseLaplaceResults
